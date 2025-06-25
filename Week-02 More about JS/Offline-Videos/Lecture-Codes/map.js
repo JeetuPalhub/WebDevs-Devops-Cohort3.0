0ce1141fe5ba3134654dd24a51cb03de5ef56287
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-06-26 01:26:02 +0530

    map.js file added

---
+++ Week-02 More about JS/Offline-Videos/Lecture-Codes/map.js
@@ -0,0 +1,7 @@
+// Creates a new array by applying a function to every element of the original array.
+
+const nums = [1, 2, 3, 4];
+
+const squares = nums.map(num => num * num);
+
+console.log(squares); // [1, 4, 9, 16]
